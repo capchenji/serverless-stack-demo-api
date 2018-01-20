af0a4a564ce8606f81783570cd24d7e35f7a79e1
Guy Lichtman <guy@guylichtman.com>
2018-01-20 10:41:38 +0200

    add support for aws xray

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -1,4 +1,7 @@
 import AWS from "aws-sdk";
+import XRay from 'aws-xray-sdk';
+
+XRay.captureAWS(AWS); //standard capture code of X-Ray to catch AWS SDK calls
 
 AWS.config.update({ region: "us-east-1" });
 
